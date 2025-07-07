e404c91e59123feea7bd2c8b3a3501aeb2df7da6
dmi3midd <dmi3midd@gmail.com>
2025-07-07 18:43:51 +0300

    Fixed locale import

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import ua from './public/locales/ua.json';
-import en from './public/locales/en.json';
+import ua from './assets/locales/ua.json';
+import en from './assets/locales/en.json';
 
 i18n
   .use(initReactI18next)
