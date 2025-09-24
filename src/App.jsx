15c289c10a68dde24f11910579a6fdccd11963fa
dmi3mid <dmi3midd@gmail.com>
2025-09-24 13:55:20 +0000

    Updated bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ export default function App() {
     <div className="relative h-min-screen w-full flex justify-center">
       <div className="fixed inset-0 -z-10">
         <img className="w-full h-full object-cover" src="src/assets/images/bg.jpg" alt="" />
-        {/* <LetterGlitch
+        <LetterGlitch
           glitchSpeed={100}
           outerVignette={false}
           glitchColors={['#414243', '#575757', '#737374']}
-        /> */}
+        />
       </div>
-      {/* <div id='lg' className="w-[80%] mt-[100px] mb-[100px] rounded-[50px] backdrop-blur-[4px] p-[50px] bg-[#161616]/10"> */}
-      <div id='img' className="w-[80%] mt-[100px] mb-[100px] rounded-[50px] backdrop-blur-[8px] p-[50px] bg-[#161616]/0">
+      <div id='lg' className="w-[80%] mt-[100px] mb-[100px] rounded-[50px] backdrop-blur-[4px] p-[50px] bg-[#161616]/10">
+      {/* <div id='img' className="w-[80%] mt-[100px] mb-[100px] rounded-[50px] backdrop-blur-[8px] p-[50px] bg-[#161616]/0"> */}
         <Header />
         <Main />
         <Footer />
